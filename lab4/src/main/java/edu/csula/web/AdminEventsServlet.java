5b374af8dca715514038daef61ca2d648fd3f549
richardshu <richardjshu@gmail.com>
2018-04-14 13:51:23 -0700

    Implement doGet() to render table of events

--- lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
+++ lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -20,16 +20,51 @@ public class AdminEventsServlet extends HttpServlet {
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
+
 		// TODO: render the events page HTML
 		EventsDAO dao = new EventsDAOImpl(getServletContext());
+
+		// Add test data
+		dao.add(new Event(1, "name", "desc", 10));
+		dao.add(new Event(2, "name2", "desc2", 20));
+
 		Collection<Event> events = dao.getAll();
-		System.out.println(events);
-		out.println("<h1>Hello events servlet!</h1>");
+		StringBuilder html = new StringBuilder();
+
+		// Create the table header
+		html.append(
+			"<table>" +
+				"<tr>" +
+					"<th>ID</th>" +
+					"<th>Name</th>" +
+					"<th>Description</th>" +
+					"<th>Trigger At</th>" +
+				"</tr>"
+		);
+
+		// Add each event to the table
+		for (Event e : events) {
+			html.append(
+				"<tr>" +
+					"<td>" + e.getId() + "</td>" +
+					"<td>" + e.getName() + "</td>" +
+					"<td>" + e.getDescription() + "</td>" +
+					"<td>" + e.getTriggerAt() + "</td>" +
+				"</tr>"
+			);
+		}
+
+		// Add the closing table tag
+		html.append("</table");
+
+		// Render the table
+		out.println(html);
 	}
 
 
 	@Override
 	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO: handle upsert transaction
+		
 	}
 }
