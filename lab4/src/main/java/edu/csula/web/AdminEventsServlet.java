5f22c32f15326e39e7f415be372bbb859ee034d7
richardshu <richardjshu@gmail.com>
2018-04-14 15:17:49 -0700

    Implement doPost() to add events to the table of events

--- lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
+++ lab4/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -23,35 +23,45 @@ public class AdminEventsServlet extends HttpServlet {
 
 		// TODO: render the events page HTML
 		EventsDAO dao = new EventsDAOImpl(getServletContext());
-
-		// Add test data
-		dao.add(new Event(1, "name", "desc", 10));
-		dao.add(new Event(2, "name2", "desc2", 20));
-
 		Collection<Event> events = dao.getAll();
 		StringBuilder html = new StringBuilder();
 
+		// Add the page header
+		html.append("<h1 id='title' class='text-center'>Incremental Game Framework</h1>");
+		html.append("<nav class='text-center'>");
+		html.append("	<ul>");
+		html.append("		<li><a href='#'><h3>Game Information</h3></a></li>");
+		html.append("		<li><a href='#'><h3>Generators</h3></a></li>");
+		html.append("		<li><a href='#''><h3>Events</h3></a></li>");
+		html.append("	</ul>");
+		html.append("</nav>");
+		html.append("<form method='POST'>");
+		html.append("	<label for='event_name'></label>");
+		html.append("	<input name='event_name' class='input-box left-margin' placeholder='Enter the event name:' required><br/>");
+		html.append("	<label for='event_description'></label>");
+		html.append("	<input name='event_description' class='input-box left-margin' placeholder='Enter the event description:' required><br/>");
+		html.append("	<label for='trigger'></label>");
+		html.append("	<input name='trigger' class='input-box left-margin' placeholder='Trigger at:' required><br/>");
+		html.append("	<button class='btn'><h4>Add</h4></button>");
+		html.append("</form>");
+
 		// Create the table header
-		html.append(
-			"<table>" +
-				"<tr>" +
-					"<th>ID</th>" +
-					"<th>Name</th>" +
-					"<th>Description</th>" +
-					"<th>Trigger At</th>" +
-				"</tr>"
-		);
+		html.append("<table>");
+		html.append("	<tr>");
+		html.append("		<th>ID</th>");
+		html.append("		<th>Name</th>");
+		html.append("		<th>Description</th>");
+		html.append("		<th>Trigger At</th>");
+		html.append("	</tr>");
 
 		// Add each event to the table
 		for (Event e : events) {
-			html.append(
-				"<tr>" +
-					"<td>" + e.getId() + "</td>" +
-					"<td>" + e.getName() + "</td>" +
-					"<td>" + e.getDescription() + "</td>" +
-					"<td>" + e.getTriggerAt() + "</td>" +
-				"</tr>"
-			);
+			html.append("<tr>");
+			html.append("	<td>" + e.getId() + "</td>");
+			html.append("	<td>" + e.getName() + "</td>");
+			html.append("	<td>" + e.getDescription() + "</td>");
+			html.append("	<td>" + e.getTriggerAt() + "</td>");
+			html.append("</tr>");
 		}
 
 		// Add the closing table tag
@@ -65,6 +75,16 @@ public class AdminEventsServlet extends HttpServlet {
 	@Override
 	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO: handle upsert transaction
-		
+
+		String name = request.getParameter("event_name");
+		String description = request.getParameter("event_description");
+		int triggerAt = Integer.parseInt(request.getParameter("trigger"));
+
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> events = dao.getAll();
+
+		dao.add(new Event(events.size(), name, description, triggerAt));
+
+		response.sendRedirect("../admin/events");
 	}
 }
