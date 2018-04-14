d44371f4e3b0f537d84ab1eedf9d65be87d0f642
richardshu <richardjshu@gmail.com>
2018-04-14 11:45:14 -0700

    Implement methods for EventsDaoImpl

--- lab4/src/main/java/edu/csula/storage/servlet/EventsDAOImpl.java
+++ lab4/src/main/java/edu/csula/storage/servlet/EventsDAOImpl.java
@@ -30,7 +30,7 @@ import edu.csula.models.Event;
  * ```
  */
 public class EventsDAOImpl implements EventsDAO {
-	private final ServletContext context;
+	private final ServletContext context; // key
 	protected static final String CONTEXT_NAME = "events";
 
 	public EventsDAOImpl(ServletContext context) {
@@ -40,28 +40,70 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public List<Event> getAll() {
 		// TODO: read a list of events from context
-		return new ArrayList<>();
+		Object data = context.getAttribute(CONTEXT_NAME);
+		if (data == null) {
+			return new ArrayList<>();
+		}
+		return (List<Event>) data;
 	}
 
 	@Override
 	public Optional<Event> getById(int id) {
 		// TODO: get a certain event given its id from context (see getAll() on
 		// getting a list first and get a certain one from the list)
+		
+		List<Event> events = getAll();
+		for (int i = 0; i < events.size(); i++) {
+			Event event = events.get(i);
+			if (event.getId() == id) {
+				return Optional.of(event);
+			}
+		}
 		return Optional.empty();
 	}
 
 	@Override
 	public void set(int id, Event event) {
 		// TODO: set a certain event given id to be different from context
+		List<Event> events = getAll();
+
+		// HINT: use a for loop
+		for (int i = 0; i < events.size(); i++) {
+			if (events.get(i).getId() == id) {
+				events.get(i).setName(event.getName());
+				events.get(i).setDescription(event.getDescription());
+				events.get(i).setTriggerAt(event.getTriggerAt());
+				break;
+			}
+		}
+
+		// HINT: Call setAttribute after mutation since the list is mutated
+		context.setAttribute(CONTEXT_NAME, events);
 	}
 
 	@Override
 	public void add(Event event) {
 		// TODO: add a new event to the context
+		List<Event> events = getAll();
+		events.add(event);
+
+		// HINT: Call setAttribute after mutation
+		context.setAttribute(CONTEXT_NAME, events);
 	}
 
 	@Override
 	public void remove(int id) {
 		// TODO: remove a single event given id
+		List<Event> events = getAll();
+
+		for (int i = 0; i < events.size(); i++) {
+			if (events.get(i).getId() == id) {
+				events.remove(i);
+				break;
+			}
+		}
+
+		// HINT: Call setAttribute after mutation
+		context.setAttribute(CONTEXT_NAME, events);
 	}
 }
