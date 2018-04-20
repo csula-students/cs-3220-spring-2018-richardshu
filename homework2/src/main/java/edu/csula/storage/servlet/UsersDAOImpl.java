d004c335482a029f098eb5927376fff1678129dd
richardshu <richardjshu@gmail.com>
2018-04-20 16:22:13 -0700

    Implement methods for UsersDaoImpl

--- homework2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
+++ homework2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
@@ -24,17 +24,28 @@ public class UsersDAOImpl implements UsersDAO {
 	public boolean authenticate(String username, String password) {
 		// TODO: check if username/password combination is valid and store the
 		//       username/password into the session
+		User user = new User(0, username, password);
+		User admin = new User(0, "admin", "cs3220password");
+		if (user.equals(admin)) {
+			context.setAttribute(CONTEXT_NAME, user);
+			return true;
+		}
 		return false;
 	}
 
 	@Override
 	public Optional<User> getAuthenticatedUser() {
 		// TODO: return the authenticated user if there is any
-		return Optional.empty();
+		Object data = context.getAttribute(CONTEXT_NAME);
+		if (data == null) {
+			return Optional.empty();
+		}
+		return Optional.of((User) data);
 	}
 
 	@Override
 	public void logout() {
-		// TOOD: log user out using `invalidate`
+		// TODO: log user out using `invalidate`
+		context.invalidate();
 	}
 }
