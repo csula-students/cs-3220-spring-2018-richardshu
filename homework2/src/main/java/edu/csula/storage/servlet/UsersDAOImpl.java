f650911c69a6e9e6bafeef5cf9db9721d20731c4
richardshu <richardjshu@gmail.com>
2018-04-20 14:06:44 -0700

    Initial commit for Homework2

---
+++ homework2/src/main/java/edu/csula/storage/servlet/UsersDAOImpl.java
@@ -0,0 +1,40 @@
+package edu.csula.storage.servlet;
+
+import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import edu.csula.models.User;
+import edu.csula.storage.UsersDAO;
+
+/**
+ * To abstract the storage access from the business layer using HttpSession
+ */
+public class UsersDAOImpl implements UsersDAO {
+	private final HttpSession context;
+	protected static final String CONTEXT_NAME = "users";
+
+	public UsersDAOImpl(HttpSession context) {
+		this.context = context;
+	}
+
+	@Override
+	public boolean authenticate(String username, String password) {
+		// TODO: check if username/password combination is valid and store the
+		//       username/password into the session
+		return false;
+	}
+
+	@Override
+	public Optional<User> getAuthenticatedUser() {
+		// TODO: return the authenticated user if there is any
+		return Optional.empty();
+	}
+
+	@Override
+	public void logout() {
+		// TOOD: log user out using `invalidate`
+	}
+}
---
+++ homework2/src/main/java/edu/csula/web/AdminEventsServlet.java
@@ -0,0 +1,50 @@
+package edu.csula.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+import edu.csula.storage.servlet.EventsDAOImpl;
+import edu.csula.storage.EventsDAO;
+import edu.csula.models.Event;
+
+@WebServlet("/admin/events")
+public class AdminEventsServlet extends HttpServlet {
+	@Override
+	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		// TODO: render the events page HTML
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> events = dao.getAll();
+
+		// Connect the servlet with the JSP file
+		request.setAttribute("events", events);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/admin-events.jsp");
+		dispatcher.forward(request, response);
+	}
+
+
+	@Override
+	public void doPost( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO: handle upsert transaction
+
+		String name = request.getParameter("event_name");
+		String description = request.getParameter("event_description");
+		int triggerAt = Integer.parseInt(request.getParameter("trigger"));
+
+		EventsDAO dao = new EventsDAOImpl(getServletContext());
+		Collection<Event> events = dao.getAll();
+
+		dao.add(new Event(events.size(), name, description, triggerAt));
+
+		response.sendRedirect("../admin/events");
+	}
+}
