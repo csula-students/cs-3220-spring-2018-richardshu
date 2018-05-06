372529f32e466c0bf3a517d25e8137a3c3146abe
richardshu <richardjshu@gmail.com>
2018-05-05 19:06:06 -0700

    Implement getAll() method in EventsDAOImpl

--- lab7/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
+++ lab7/src/main/java/edu/csula/storage/mysql/EventsDAOImpl.java
@@ -3,6 +3,7 @@ package edu.csula.storage.mysql;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.sql.*;
 
 import edu.csula.storage.EventsDAO;
 import edu.csula.storage.Database;
@@ -11,7 +12,7 @@ import edu.csula.models.Event;
 public class EventsDAOImpl implements EventsDAO {
 	private final Database context;
 
-	// TODO: fill the Strings with the SQL queries as "prepated statements" and
+	// TODO: fill the Strings with the SQL queries as "prepared statements" and
 	//       use these queries variable accordingly in the method below
 	protected static final String getAllQuery = "";
 	protected static final String getByIdQuery = "";
@@ -26,7 +27,21 @@ public class EventsDAOImpl implements EventsDAO {
 	@Override
 	public List<Event> getAll() {
 		// TODO: get all events from jdbc
-		return new ArrayList<>();
+		List<Event> events = new ArrayList<>();
+		try (Connection c = context.getConnection(); Statement stmt = c.createStatement()) {
+			ResultSet rs = stmt.executeQuery("SELECT * FROM Events;");
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String description = rs.getString(3);
+				int triggerAt = rs.getInt(4);
+				events.add(new Event(id, name, description, triggerAt));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return new ArrayList<>();
+		}
+		return events;
 	}
 
 	@Override
